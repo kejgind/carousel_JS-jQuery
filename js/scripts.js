2d14cf0145e6a2528e67d466327871c91995a8e1
Krzysiek <kejgi@op.pl>
2017-08-25 21:53:18 +0200

    functions tidying

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
+// scripts.js
+
 $(document).ready(function(){
   "use strict";
 
-  // wyliczenie długości całej listy w zależności od ilości zdjęć
-  // var galleryLength = $('ul').width($('ul').find('li').length * 800);
-  // console.log(galleryLength);
-
+  // deklaracja zmiennych
   var carouselList = $('#carousel ul');
 
-  var imgPause = 5000;
-  var imgPauseOnClick = 500;
-  var animationTime = 1500;
+  var imgPause = 3000;
+  var animationTime = 1200;
 
   var marginZero = 0;
-  var marginOne = 800;
+  var marginOne = 800; // wartość równa przyjętej w CSS szerokości obrazka
 
   var firstItem;
   var lastItem;
+
+  var interval;
+
+  // wyliczenie długości całej listy w zależności od ilości zdjęć
+  var galleryLength = $('ul').width($('ul').find('li').length * marginOne);
+  console.log(galleryLength);
 
   // przesunięcie obrazka w lewo
   var moveFirstSlide = function(){
@@ -24,8 +28,8 @@ $(document).ready(function(){
     lastItem.after(firstItem);
     carouselList.css({marginLeft:marginZero});
   };
-
-  // przesunięcie obrazka w prawo
+  
+    // przesunięcie obrazka w prawo
   var moveLastSlide = function(){
     firstItem = carouselList.find('li:first');
     lastItem = carouselList.find('li:last');
@@ -33,48 +37,35 @@ $(document).ready(function(){
     carouselList.css({marginLeft:-marginOne});
   };
 
-  // automatyczne zmiana zdjęcia
-  var interval;
+  // automatyczna zmiana zdjęcia
+  function moveSlideForward (){
+    carouselList.animate({'marginLeft':'-='+marginOne}, animationTime, moveFirstSlide);
+  }
+
   function autoAnimation(){
-    interval = setInterval(function(){
-      carouselList.animate({'marginLeft':'-='+marginOne}, animationTime, moveFirstSlide);},
-      imgPause);
-  };
+    interval = setInterval(moveSlideForward, imgPause);
+  }
   autoAnimation();
 
   // zatrzymanie automatycznej zmiany zdjęcia po najechaniu myszką na obrazek
   function stopAutoAnimation(){
     clearInterval(interval);
-  };
+  }
 
   $('#carousel').on('mouseenter', stopAutoAnimation).on('mouseleave', autoAnimation);
 
-  // podświetlenie strzałek i przesunięcie zdjęcia
-  // $('.arrow').on('mouseenter', function(){
-  //   $(this).css('opacity', 1);}
-  // ).on('mouseleave', function(){
-  //   $(this).css('opacity', 0.4);});
-
-  $('.fa-chevron-right').on('click', function(){
-    setTimeout(function(){
-      carouselList.animate({
-        'marginLeft':-marginOne
-      }, animationTime, moveFirstSlide);
-    }, imgPauseOnClick);
-  });
+  // przesuwanie slajdów po kliknięciu na strzałkę
+  function moveSlideBackwards (){
+    carouselList.animate({'marginLeft':marginZero}, animationTime, moveLastSlide);
+  }
 
   $('.fa-chevron-left').on('click', function(){
-    setTimeout(function(){
-      carouselList.animate({
-        'marginLeft':marginZero
-      }, animationTime, moveLastSlide);
-    }, imgPauseOnClick);
+    moveLastSlide();
+    moveSlideBackwards();
   });
 
-  var imgShown = function(){
-    carouselList.find('li:first').html();
-  };
-
-  console.log(imgShown);
+  $('.fa-chevron-right').on('click', function(){
+    moveSlideForward();
+  });
 
 });
