638ac728777e1b4a05a73496f63b58966cc4bb3f
Krzysztof G <kejgind@gmail.com>
2018-05-17 13:54:16 +0200

    grab images from DOM, create bullets, change images with buttons, show on bullet which image is shown right now

--- js/scripts.js
+++ js/scripts.js
@@ -1,114 +1,89 @@
 // scripts.js
 
-$(document).ready(function(){
-  "use strict";
+// grab elements from DOM, list of images, buttons, etc.
+const elementsFromDOM = {
+  carousel: document.getElementById("carousel"),
+  carouselList: document.querySelector("#carousel ul"),
+  galleryImages: Array.from(document.querySelectorAll("#carousel li")),
+  prevSlide: document.querySelector(".fa-chevron-left"),
+  nextSlide: document.querySelector(".fa-chevron-right"),
+  bulletsContainer: document.querySelector(".carousel-bullets"),
+};
 
-  // (EN) variables declaration | (PL) deklaracja zmiennych
-  var carouselList = $('#carousel ul');
-
-  // (EN) declaration of variable responsible for checking, if there is animation in progress | (PL) ustawienie zmiennej odpowiedzialnej za sprawdzenie, czy aktualnie trwa animacja zmiany obrazka
-  var isAnimating = false;
-
-  // (EN) setting time for image display and animation duration | (PL) ustawienie czasu wyświetlania obrazka oraz czasu animacji przejścia
-  var imgPause = 2000;
-  var animationTime = 1000;
-
-  // | (PL) określenie wartości dla przesuwania galerii obrazków
-  var marginZero = 0;
-  var marginOne = 800; // wartość równa przyjętej w CSS szerokości obrazka
-
-  // | (PL) wartość równa przyjętej w CSS szerokości obrazka
-  var galleryImagesCount = $('#carousel li').length;
-  var galleryLength = galleryImagesCount * marginOne;
-  console.log(galleryImagesCount, galleryLength);
-
-  // | (PL) zdefiniowanie indexu wyświetlanego obecnie obrazka z galerii
-  var displayedImageIndex = 0;
-
-  var firstItem;
-  var lastItem;
-  var interval;
-
-  // | (PL) przesunięcie obrazka w lewo
-  var moveFirstSlide = function(){
-    firstItem = carouselList.find('li:first');
-    lastItem = carouselList.find('li:last');
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:marginZero});
-  };
-
-  // | (PL) przesunięcie obrazka w prawo
-  var moveLastSlide = function(){
-    firstItem = carouselList.find('li:first');
-    lastItem = carouselList.find('li:last');
-    firstItem.before(lastItem);
-    carouselList.css({marginLeft:-marginOne});
-  };
-
-  // | (PL) automatyczna zmiana zdjęcia
-  function moveSlideForward (){
-    if (isAnimating == true){
-      return;
-    } else {
-      isAnimating = true;
-      carouselList.animate({'marginLeft':'-='+marginOne}, animationTime, moveFirstSlide);
-      // | (PL) sprawdzanie i ustawianie nr wyświetlanego obrazka przy zmianie obrazka w przód
-      displayedImageIndex = (displayedImageIndex + 1) % galleryImagesCount;
-      changeBulletIcon();
-      // console.log(displayedImageIndex);
-      isAnimating = false;
-    };
-  }
-
-  function autoAnimation(){
-    interval = setInterval(moveSlideForward, imgPause);
-  }
-  autoAnimation();
-
-  // | (PL) zatrzymanie automatycznej zmiany zdjęcia po najechaniu myszką na obrazek
-  function stopAutoAnimation(){
-    clearInterval(interval);
-  }
-  $('#carousel').on('mouseenter', stopAutoAnimation).on('mouseleave', autoAnimation);
-
-  // | (PL) przesuwanie slajdów wstecz
-  function moveSlideBackwards (){
-    if (isAnimating == true){
-      return;
-    } else {
-      isAnimating = true;
-      moveLastSlide();
-      carouselList.animate({'marginLeft':marginZero}, animationTime);
-      // | (PL) sprawdzanie i ustawianie nr wyświetlanego obrazka przy zmianie obrazka w tył
-      displayedImageIndex = (displayedImageIndex - 1) % galleryImagesCount;
-      if (displayedImageIndex < 0){
-        displayedImageIndex = galleryImagesCount - 1;
-      };
-      changeBulletIcon();
-      // console.log(displayedImageIndex);
-      isAnimating = false;
-    };
-  };
-
-  // | (PL) przesuwanie slajdów po kliknięciu na strzałkę
-  $('.fa-chevron-left').on('click', function(){
-    moveSlideBackwards();
+// create bullets
+const addBullets = (gallery, container) => {
+  const bullets = gallery.map((image, index) => {
+    image.classList.add(`image-${index}`);
+    return `<i class="fa fa-circle-thin bullet" data-image="image-${index}"></i>`;
   });
-  $('.fa-chevron-right').on('click', function(){
-    moveSlideForward();
+  container.innerHTML = bullets
+    .join(" ")
+    .replace("fa-circle-thin", "fa-circle")
+    .replace(/,/g, "");
+  return Array.from(container.children);
+};
+
+const bulletsGlobal = addBullets(
+  elementsFromDOM.galleryImages,
+  elementsFromDOM.bulletsContainer
+);
+
+console.log(bulletsGlobal);
+
+// set things that can be altered
+const configuration = {
+  imgDisplayTime: 2000, // set duration to 2 seconds
+  animationTime: 1000, // set animation duration to 1 sec
+};
+
+const otherSettings = {
+  startPosition: 0,
+  endPosition: elementsFromDOM.galleryImages[0].offsetWidth, // value equal to image width set in css file
+  galleryImagesCount: elementsFromDOM.galleryImages.length,
+  displayedImageIndex: 0,
+  isAnimating: false,
+};
+
+const galleryLength =
+  otherSettings.endPosition * otherSettings.galleryImagesCount;
+
+const moveSlideLeft = () => {
+  let gallery = elementsFromDOM.galleryImages;
+  let elList = elementsFromDOM.carouselList;
+  elList.style.marginLeft = `${-otherSettings.endPosition}px`;
+  const firstElem = gallery.shift();
+  elList.removeChild(firstElem);
+  elList.style.marginLeft = `${otherSettings.startPosition}px`;
+  gallery.push(firstElem);
+  elList.insertAdjacentElement("beforeend", firstElem);
+  setActiveBullet(gallery[0].classList[0]);
+  return gallery;
+};
+
+const moveSlideRight = () => {
+  let gallery = elementsFromDOM.galleryImages;
+  let elList = elementsFromDOM.carouselList;
+  const lastElem = gallery.pop();
+  elList.removeChild(lastElem);
+  gallery.unshift(lastElem);
+  elList.insertAdjacentElement("afterbegin", lastElem);
+  elList.style.marginLeft = `${otherSettings.startPosition}px`;
+  setActiveBullet(gallery[0].classList[0]);
+  return gallery;
+};
+
+const setActiveBullet = currentImage => {
+  const bullets = bulletsGlobal;
+  const bullet = bullets.filter(el => el.dataset.image === currentImage)[0];
+  bullets.map(el => {
+    el.classList.remove("fa-circle");
+    el.classList.add("fa-circle-thin");
   });
+  bullet.classList.remove("fa-circle-thin");
+  bullet.classList.add("fa-circle");
+};
 
-  // | (PL) wskazanie (na liście kropek) aktualnie wyświetlanego zdjęcia
-  function changeBulletIcon(){
-    $('i.bullet').removeClass('fa-circle').addClass('fa-circle-thin')
-      .eq(displayedImageIndex)
-      .removeClass('fa-circle-thin').addClass('fa-circle');
-  };
 
-  // | (PL) sprawdzenie nr klikniętego elementu wyboru zdjęcia
-  var bulletIconClicked = $('i.bullet').click(function(){
-    bulletIconClicked = $(this).index();
-    console.log(bulletIconClicked);
-  });
 
-});
+elementsFromDOM.prevSlide.addEventListener("click", moveSlideLeft);
+elementsFromDOM.nextSlide.addEventListener("click", moveSlideRight);
